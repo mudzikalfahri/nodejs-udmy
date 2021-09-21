cfa6f16260c7aecd686b325d893a54c4b17613fa
unknown <alfahrimudzik@gmail.com>
2021-09-21 21:12:26 +0700

    express

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
-const http = require("http");
-const routes = require("./routes");
-const server = http.createServer(routes);
+const express = require("express");
+const app = express();
 
-server.listen(3000);
+app.use((req, res, next) => {
+  console.log("middleware");
+});
+
+app.listen(3000);
