e261fcadb45c4a5737ce62a02b0c4adcd3bb07e4
unknown <alfahrimudzik@gmail.com>
2021-10-03 13:38:31 +0700

    connect to db

--- app.js
+++ app.js
@@ -1,8 +1,19 @@
 const express = require("express");
 const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const productRoute = require("./routes/product");
 
-app.use((req, res, next) => {
-  console.log("middleware");
-});
+dotenv.config();
+
+mongoose
+  .connect(process.env.MONGODB_URL)
+  .then(() => console.log("connected to db"))
+  .catch((err) => {
+    console.log(err);
+  });
+
+app.use(express.json());
+app.use("/api/products", productRoute);
 
 app.listen(3000);
---
+++ routes/product.js
@@ -0,0 +1,15 @@
+const Product = require("../models/product");
+const router = require("express").Router();
+
+router.post("/", async (req, res) => {
+  const newProduct = new Product(req.body);
+
+  try {
+    const savedProduct = await newProduct.save();
+    res.status(200).json(savedProduct);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
+
+module.exports = router;
