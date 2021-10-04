a82482e88a66af7be1ac68fbbc0de56a34660baa
unknown <alfahrimudzik@gmail.com>
2021-10-04 22:48:35 +0700

    user route

--- routes/product.js
+++ routes/product.js
@@ -3,13 +3,16 @@ const router = require("express").Router();
 
 router.post("/", async (req, res) => {
   const newProduct = new Product(req.body);
-
   try {
     const savedProduct = await newProduct.save();
     res.status(200).json(savedProduct);
   } catch (err) {
     res.status(500).json(err);
   }
+});
+
+router.get("/", (req, res) => {
+  res.send("product route");
 });
 
 module.exports = router;
